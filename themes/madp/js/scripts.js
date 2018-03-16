8a64d2d2c2a8beee83b2d6d779acd644dd8d986e
Artem Goncharov <iskymen@gmail.com>
2018-03-16 09:51:15 +0200

    New taxonomy menu3

--- themes/madp/js/scripts.js
+++ themes/madp/js/scripts.js
@@ -18,8 +18,12 @@ Drupal.behaviors.tabs = {
         attach: function (context, settings) {
             var $activeItem = $('.hierarchical-taxonomy-menu .menu-item--active');
             var $parentActive = $activeItem.parent().parent();
-            var $parentActiveIndex = $('.hierarchical-taxonomy-menu > .menu-item').index($parentActive);
-
+            var $parentActiveIndex = (($activeItem.length > 0)
+                ? $('.hierarchical-taxonomy-menu > .menu-item').index($parentActive)
+                : 0
+            );
+            console.log($activeItem);
+            console.log($parentActiveIndex);
             $('.hierarchical-taxonomy-menu').accordion({
                 animate: 200,
                 active: $parentActiveIndex
